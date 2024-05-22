171e14a5cf4682d2f6d31c750a874f38e91aeb2e
unknown <105841110321@student.unismuh.ac.id>
2024-05-22 16:43:58 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react' 
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+const App = () => {
+  return (
+  <View style={{flex:1}}>
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: 'green' }}></View>
+      <View style={{ flex: 1, backgroundColor: 'yellow' }}></View>
+    </View>
+    <View style={{ flex: 1, flexDirection: "column" }}>
+      <View style={{ flex: 1, backgroundColor: 'red' }}></View>
+      <View style={{ flex: 1, backgroundColor: 'blue' }}></View>
+    </View>
+    </View>
+  
+  )
+}
+export default App
