0ec01fd52e9e8c9bafa827f2300e82dbe06420de
unknown <105841110321@student.unismuh.ac.id>
2024-07-30 21:49:12 +0800

    usehook data and onchange

--- App2.js
+++ App2.js
@@ -1,36 +1,24 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native'
-import React, { useState } from 'react'
-import { Button } from 'react-native-web'
-const App = () => {
-  const [formLogin, setForm] = useState({
-    email: '',
-    password: ''
-  })
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Login from './component/pages/login'
+import ForgotPassword from './component/pages/forgotPassword'
+import SignUp from './component/pages/signUp'
+import MainPage from './component/pages/mainPage'
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Form Login</Text>
-      <View>
-        <Text>Email</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={(hasil) => setForm({ ...formLogin, email: hasil })}
-          value={formLogin.email}
-        />
-        <Text>Password</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={(text) => setForm({ ...formLogin, password: text })}
-          value={formLogin.password}
-        />
-        <View style={{ marginTop: 10 }}>
-          <Button title="Login" onPress={() => alert('Login')} />
-        </View>
-        <View>
-          <Text>Email: {formLogin.email}</Text>
-          <Text>Password: {formLogin.password}</Text>
-        </View>
-      </View>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "login">
+        <Stack.Screen name="login" component={Login} options = {{headerShown: false }} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} options = {{headerShown: false }} />
+        <Stack.Screen name="signUp" component={SignUp} options = {{headerShown: false }} />
+        <Stack.Screen name="mainPage" component={MainPage} options = {{headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-export default App
+
+export default App;
